998c7aec089e25ed472160b6fd3e54d182cc2b2a
Atiyeh Ahmadi <a94ahmad@uwaterloo.ca>
2022-04-11 03:12:06 -1200

    processing

--- FAST/Schnitzcells sample images set/1. Raw Images/post-processing/scripts/FAST_Bacteria_Features.m
+++ FAST/Schnitzcells sample images set/1. Raw Images/post-processing/scripts/FAST_Bacteria_Features.m
@@ -8,6 +8,9 @@ cellNumber_value = [];
 orientation=[];
 Center_X=[];
 Center_Y=[];
+major_axis=[];
+minor_axis=[];
+
 
 
 for i=1:cellNumber
@@ -18,14 +21,16 @@ for i=1:cellNumber
         orientation (end+1) = procTracks(i).phi(j); 
         Center_X (end+1) = procTracks(i).x(j); 
         Center_Y (end+1) = procTracks(i).y(j); 
+        major_axis (end+1) = procTracks(i).majorLen(j); 
+        minor_axis (end+1) = procTracks(i).minorLen(j); 
     end
 end
 
 
 %add to sorted table
-T = sortrows(table(transpose(TimeStep),transpose(cellNumber_value),transpose(orientation),transpose(Center_X),transpose(Center_Y)));
+T = sortrows(table(transpose(TimeStep),transpose(cellNumber_value),transpose(orientation),transpose(Center_X),transpose(Center_Y),transpose(major_axis),transpose(minor_axis)));
 %add column name
-T.Properties.VariableNames={'TimeStep','CellNumber','Orientation','Center_X','Center_Y'};
+T.Properties.VariableNames={'TimeStep','CellNumber','Orientation','Center_X','Center_Y','Major_axis','Minor_axis'};
 
 % write to csv
 writetable(T,'../results/FAST_bacteria_feature_analysis.csv','Delimiter',',','QuoteStrings',true)
