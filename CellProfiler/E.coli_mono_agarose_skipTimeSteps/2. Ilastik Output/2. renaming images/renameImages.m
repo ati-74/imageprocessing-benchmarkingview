2989225fa45505cada5f253d283fbc5200dce7fb
Atiyeh Ahmadi <a94ahmad@uwaterloo.ca>
2022-05-23 04:46:08 -0700

    one images out of every 10 is processed.

---
+++ CellProfiler/E.coli_mono_agarose_skipTimeSteps/2. Ilastik Output/2. renaming images/renameImages.m
@@ -0,0 +1,21 @@
+%find .mat files in cell directory
+directory='../1. Skip TimeSteps/';
+files=dir([strcat(directory,'*.tif')]);
+%sorted files
+sorted_files = natsortfiles(files);
+%number of files
+Num_files=length(sorted_files);
+
+for i=1:Num_files
+    file_name=sorted_files(i).name;
+    bw = imread(strcat(directory,file_name));
+    num_digit = numel(num2str(i));
+    if num_digit ==1
+        img_name = strcat('final/E.coli_mono_agarose_T00', int2str(i),'.tif');
+    elseif num_digit ==2
+           img_name = strcat('final/E.coli_mono_agarose_T0', int2str(i),'.tif'); 
+    else
+        img_name = strcat('final/E.coli_mono_agarose_T', int2str(i),'.tif'); 
+    end
+    imwrite(bw,img_name);
+end
